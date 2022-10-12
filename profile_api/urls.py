b3514b82b3e09a7b10c90bc7e5d31b9a54981819
chandan soni <chandansoni115@gmail.com>
2022-10-12 17:53:52 +0530

    added viewpost

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.db import router
+from django.urls import path,include
+
+from rest_framework.routers import DefaultRouter
+
 from profile_api import views
 
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSets, basename='hello-viewset')
+
 urlpatterns =[
     path('hello-view/',views.HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
